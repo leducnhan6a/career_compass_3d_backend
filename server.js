28b258a3cd146b5aeb2ab64d65e7dd3b4df0ca77
leducnhan6a <taixegacon4a@gmail.com>
2025-04-14 23:58:59 +0700

    first commit server.js file

---
+++ server.js
@@ -0,0 +1,13 @@
+import app from './src/app.js';
+
+const PORT = process.env.DEV_APP_PORT;
+
+const server = app.listen(PORT, () => {
+    console.log(`Webdev Career Compass 3D start with url http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', () => {
+    server.close(() => console.log('exit server express'));
+    // window.clearInterval()
+    // notify.send(ping...)
+});
