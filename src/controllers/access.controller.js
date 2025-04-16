cc7e244a38ca454e0b654646de8eee0eafaa6572
NSUT-DDDuy <xx.shark.xx274@gmail.com>
2025-04-16 08:57:17 +0700

    Fix route name and asyncHandler in access.controller

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,32 +1,31 @@
 'use strict';
 
-import asyncHandler from '../helpers/asyncHandler.js';
 import AccessService from '../services/access.service';
 import { CREATED, SuccessResponse } from '../core/success.response.js';
 
 class AccessController {
-    signup = asyncHandler(async (req, res) => {
+    async signup (req, res) {
         const result = await AccessService.signUp(req.body);
         new CREATED({
             message: 'Registered successfully!',
             metadata: result,
         }).send(res);
-    });
+    };
 
-    login = asyncHandler(async (req, res) => {
+    async login (req, res) {
         const result = await AccessService.logIn(req.body);
         new SuccessResponse({
             metadata: result,
         }).send(res);
-    });
+    };
 
-    logout = asyncHandler(async (req, res) => {
+    async logout (req, res) {
         const result = await AccessService.logOut(req.keyStore);
         new SuccessResponse({
             message: 'Logged out',
             metadata: result,
         }).send(res);
-    });
+    };
 }
 
 export default new AccessController();
